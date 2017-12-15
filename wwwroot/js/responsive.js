13f71db147bda5c4d7cc76a0dbd4e929e7609aba
Vi Cuong <tranvicuongit@gmail.com>
2017-12-15 11:42:18 +0700

    complete

---
+++ wwwroot/js/responsive.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    responsive();
+});
+$(window).resize(function (e) {
+    responsive();
+    if(!($('html').hasClass('mobile'))){
+        if($(window).width()<768 )
+        {
+            $('html').addClass('mobile').removeClass('desktop');
+            if($(window).width()<320){
+                $('html').removeClass('tablet').addClass('phone');
+            }
+            else{
+                $('html').addClass('tablet').removeClass('phone');
+            }
+        }
+        else{
+            $('html').addClass('desktop').removeClass('mobile').removeClass('tablet').removeClass('phone');
+        }
+    }
+});
+function responsive() {
+    var md = new MobileDetect(window.navigator.userAgent);
+    //Desktop
+    if (md.phone() === null && md.tablet() === null) {
+        $('html').addClass('desktop').removeClass('mobile').removeClass('tablet').removeClass('phone');
+    }
+    //Mobile
+    else {
+        $('html').addClass('mobile').removeClass('desktop');
+        if (md.phone())
+        $('html').addClass('phone').removeClass('tablet');
+        else
+        $('html').addClass('tablet').removeClass('phone');
+    }
+
+    //browser
+    var ua = navigator.userAgent.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
+    var browser='';
+    if (navigator.userAgent.match(/Edge/i) || navigator.userAgent.match(/Trident.*rv[ :]*11\./i)) {
+        browser = "msie";
+    }
+    else {
+        browser = ua[1].toLowerCase();
+        if(browser ==='safari')
+        $('html').addClass('safari');
+        else
+        $('html').removeClass('safari');
+    }
+
+    if($(window).width() < 576 )
+    {   
+        $(".button-download--img").each(function(){
+            $(this).attr("src", $(this).data("srcmain") + "-mb.png" );
+        });        
+    }
+    else{
+        $(".button-download--img").each(function(){
+            $(this).attr("src", $(this).data("srcmain") + ".png" );
+        });
+    }
+};
